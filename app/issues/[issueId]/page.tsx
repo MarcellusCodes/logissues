f2df745192a569f3165bda9354e20d923daf9ad8
MarcellusCodes <MarcellusCodes@gmail.com>
2023-04-22 14:44:45 +0200

    setup xata, issues and issue filter query

--- app/issues/[issueId]/page.tsx
+++ app/issues/[issueId]/page.tsx
@@ -1,10 +1,47 @@
 import IssueEditor from "@/components/issue-editor";
+import { getXataClient } from "@/lib/xata";
 
-export default function Page({ params: { i } }: { params: { i: string } }) {
+export const metadata = {
+  title: "Issue",
+};
+
+type TIssue = {
+  id: string;
+  title: string;
+  description: string;
+  markdown: string;
+  board_id: string;
+};
+
+async function getIssue(id: string) {
+  const xata = getXataClient();
+  const issue: TIssue[] = await xata.db.issues
+    .filter({
+      id: id,
+    })
+    .getMany();
+  if (!issue) {
+    return null;
+  }
+  return issue[0];
+}
+
+export default async function Page({
+  params: { issueId },
+}: {
+  params: { issueId: string };
+}) {
+  const issue = (await getIssue(issueId)) as TIssue;
+
   return (
     <>
       <div>
-        <p>Issue {i}</p>
+        <p>Issue {issueId}</p>
+        <ul>
+          <li>{issue.title}</li>
+          <li>{issue.description}</li>
+          <li>{issue.markdown}</li>
+        </ul>
         <IssueEditor />
       </div>
     </>
--- app/issues/layout.tsx
+++ app/issues/layout.tsx
@@ -1,17 +1,47 @@
 import Link from "next/link";
 import { ReactNode } from "react";
 
+import { getXataClient } from "@/lib/xata";
+
 export const metadata = {
   title: "Issues",
 };
 
-export default function Layout({ children }: { children: ReactNode }) {
+type TIssue = {
+  id: string;
+  title: string;
+  description: string;
+  markdown: string;
+  board_id: string;
+};
+
+async function getIssues() {
+  const xata = getXataClient();
+  const issues: TIssue[] = await xata.db.issues.getMany();
+  if (!issues) {
+    return null;
+  }
+  return issues;
+}
+
+export default async function Layout({ children }: { children: ReactNode }) {
+  const issues = (await getIssues()) as TIssue[];
+  console.log(issues);
   return (
     <>
       <p>Issues</p>
-      <Link href="/issues/1">Issue 1</Link>
-      <Link href="/issues/2">Issue 2</Link>
-      <Link href="/issues/3">Issue 3</Link>
+      <div className="flex flex-col gap-3 items-start">
+        {issues.map((issue) => (
+          <Link
+            key={issue.id}
+            href={`/issues/${issue.id}`}
+            className="border border-purple-600 px-12 py-3 bg-purple-300 text-white"
+          >
+            {issue.title}
+          </Link>
+        ))}
+      </div>
+
       {children}
     </>
   );
